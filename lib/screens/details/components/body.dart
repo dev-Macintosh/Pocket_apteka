627d581367b6fe3a93767792ffb3e11af7436608
dev-Macintosh <ivan.200512340@gmail.com>
2023-05-04 14:38:05 +0300

    add imagesrc for detail-page

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -7,15 +7,15 @@ import 'images_and_icons.dart';
 import 'title_and_price.dart';
 
 class Body extends StatelessWidget {
-  const Body({super.key});
-
+  const Body({super.key,this.imageSrc=""});
+  final String imageSrc;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[
-          ImageAndIcons(size: size),
+          ImageAndIcons(size: size,imageSrc: imageSrc,),
           TitleAndPrice(title: "Angelica", country: "Russia", price: 440),
           SizedBox(height: kPaddingOffset),
           Row(
